e4e76cb73f85a60e4b89990f068e3329037ef163
renushri2010 <90689502+renushri2010@users.noreply.github.com>
2022-03-17 11:52:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio : true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, gotResults)
+{
+    console.log('Got Results');
+}
